7816ee5b76a74382ae67650b7527984af5df4c60
fakaha <lunastra34@gmail.com>
2023-07-12 17:50:18 +0700

    first push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Kennn',
-            'email' => 'kenedifp@gmail.com',
+            'name' => 'azrus',
+            'email' => 'azrus@gmail.com',
             'password' => Hash::make('123')
         ]);
     }
